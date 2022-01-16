1555c1d343fa33185e71e6d6c3c44296020bd2b0
James Domingo <domango1214@gmail.com>
2022-01-16 14:23:37 +0800

    feat: add home and 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import { Component, createEffect, For, JSX, lazy } from 'solid-js';
-import { createRouterTree, Router } from 'solid-tiny-router';
+import { createRouterTree, Router, Link } from 'solid-tiny-router';
 import ResultCard from './components/ResultCard';
 import { useFetchResults } from './models/FetchResults';
 import { TestContext } from './models/testContext';
+import NotFoundSVG from './images/404.svg'
 
 const routes = createRouterTree([
   {
@@ -15,31 +16,34 @@ const routes = createRouterTree([
   }
 ])
 
+function NotFound(): JSX.Element {
+  return (
+    <div class="container mx-auto my-10 text-center text-purple-500 font-semibold text-2xl md:text-6xl space-y-6">
+      <h1>Page not found</h1>
+      <img src={NotFoundSVG} alt="Not found" class="w-1/3 mx-auto"/>
+      <Link href="/" class='text-lg underline underline-offset-3 text-purple-400'>Go back to Home page</Link>
+    </div> 
+  )
+}
+
+function Footer(): JSX.Element {
+  return (
+    <footer class='text-center text-purple-400 mt-36'>
+      <p>{`© 2022-${new Date().getFullYear()} James Domingo - All Rights Reserved.`}</p>
+    </footer>
+  )
+}
+
 export function App(): JSX.Element{
   const result = useFetchResults();
   createEffect(() => {
     console.log(result())
   })
   return (
-    // <div class="container mx-auto text-center">
-    //   <h1>Results</h1>
-    //   <ul>
-    //     {/* <For each={result()}>
-    //       {
-    //         (result) =>    <ResultCard />
-    //       }
-    //     </For> */}
-    //     <ResultCard />
-
-    //   </ul>
-
-
-    // </div>
-<TestContext.Provider value={'hello'}>
-    <div class="container">
-      <Router routes={routes} />
-    </div>
-</TestContext.Provider>
+    <>
+      <Router routes={routes} fallback={NotFound} />
+      <Footer />
+    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,11 @@
 import { JSX } from "solid-js"
-import ResultCard from "../components/ResultCard"
+import WelcomeSVG from '../images/welcome.svg'
 
 export default function main(): JSX.Element {
   return (
-    <ResultCard />
+    <div className="mx-auto text-purple-500 text-center p-10 space-y-10">
+      <h1 class="text-2xl md:text-7xl">This is the Results Client</h1>
+      <img src={WelcomeSVG} alt="Welcome" class="w-96 mx-auto" />
+    </div>
   )
 }
